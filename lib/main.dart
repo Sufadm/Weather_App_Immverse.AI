025f8cfac697618aabb69305c99335bbb1ff2f5b
sufad M <sufadm77@gmail.com>
2024-02-08 16:42:38 +0530

    theme issue solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/model/search_location_model.dart';
 import 'package:weather_app/model/theme_model.dart';
 import 'package:weather_app/controller/weather_service_controller.dart';
@@ -8,14 +9,23 @@ import 'package:weather_app/model/weather_ui_model.dart';
 import 'package:weather_app/view/page/home_page.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
+  bool darkTheme = isFirstLaunch ? true : prefs.getBool('darkTheme') ?? false;
+
   await dotenv.load(fileName: ".env");
+  if (isFirstLaunch) {
+    await prefs.setBool('isFirstLaunch', false);
+  }
+
   runApp(
     MultiProvider(
       providers: [
         Provider<WeatherService>(create: (_) => WeatherService()),
         ChangeNotifierProvider<WeatherUI>(create: (_) => WeatherUI()),
         ChangeNotifierProvider(
-          create: (context) => DarkThemeProvider(true),
+          create: (context) => DarkThemeProvider(darkTheme),
         ),
         ChangeNotifierProvider(
           create: (context) => SearchLocationModel(),
--- lib/model/theme_model.dart
+++ lib/model/theme_model.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class DarkThemeProvider with ChangeNotifier {
   DarkThemeProvider(this._darkTheme);
-
+  bool light = false;
   bool _darkTheme;
 
   bool get darkTheme => _darkTheme;
@@ -17,5 +17,6 @@ class DarkThemeProvider with ChangeNotifier {
   Future<void> _saveThemePreference(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('darkTheme', value);
+    value = true;
   }
 }
